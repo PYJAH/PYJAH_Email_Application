dc6a33012d8c1e824b3364cd899c6fe4b6c4d1a5
Howie Huntington <rhuntington2716@gmail.com>
2018-03-17 07:39:32 -0400

    Project layout with working client gui
    GUI has some functionality, has the ability to switch scenes in the gui when pressing different buttons

---
+++ PYJAH_Software_Project/src/pyjah/client/pkg/Gui.java
@@ -0,0 +1,37 @@
+package pyjah.client.pkg;
+
+import javafx.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Gui extends Application {
+
+	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//login scene
+			primaryStage.setTitle("PYJAH Email Client");
+			Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+			Scene login = new Scene(root);
+			primaryStage.setScene(login);
+			primaryStage.show();
+			
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
